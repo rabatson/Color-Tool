654836c4d9facb9b95e2193631678d754a6d0e48
Riley <59605363+rabatson@users.noreply.github.com>
2025-02-18 11:32:28 -0600

    Initial Commit
    Putting this up on Github now

---
+++ ColorPicker/MainForm.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace ColorPicker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.colorPreview = new System.Windows.Forms.Panel();
+            this.openColorDialog = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.rDisplay = new System.Windows.Forms.TextBox();
+            this.rLabel = new System.Windows.Forms.Label();
+            this.gLabel = new System.Windows.Forms.Label();
+            this.gDisplay = new System.Windows.Forms.TextBox();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.bDisplay = new System.Windows.Forms.TextBox();
+            this.hexLabel = new System.Windows.Forms.Label();
+            this.hexDisplay = new System.Windows.Forms.TextBox();
+            this.randomColor = new System.Windows.Forms.Button();
+            this.saveColor = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.rSlider = new System.Windows.Forms.TrackBar();
+            this.gSlider = new System.Windows.Forms.TrackBar();
+            this.bSlider = new System.Windows.Forms.TrackBar();
+            this.colorPreview.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorPreview
+            // 
+            this.colorPreview.BackColor = System.Drawing.Color.Black;
+            this.colorPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorPreview.Controls.Add(this.openColorDialog);
+            this.colorPreview.Location = new System.Drawing.Point(139, 7);
+            this.colorPreview.Name = "colorPreview";
+            this.colorPreview.Size = new System.Drawing.Size(332, 110);
+            this.colorPreview.TabIndex = 0;
+            // 
+            // openColorDialog
+            // 
+            this.openColorDialog.BackColor = System.Drawing.Color.Transparent;
+            this.openColorDialog.FlatAppearance.BorderSize = 0;
+            this.openColorDialog.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.openColorDialog.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.openColorDialog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openColorDialog.ForeColor = System.Drawing.Color.Transparent;
+            this.openColorDialog.Location = new System.Drawing.Point(0, -1);
+            this.openColorDialog.Name = "openColorDialog";
+            this.openColorDialog.Size = new System.Drawing.Size(330, 109);
+            this.openColorDialog.TabIndex = 0;
+            this.openColorDialog.UseVisualStyleBackColor = false;
+            this.openColorDialog.Click += new System.EventHandler(this.openColorDialog_Click);
+            // 
+            // colorDialog
+            // 
+            this.colorDialog.FullOpen = true;
+            // 
+            // rDisplay
+            // 
+            this.rDisplay.Location = new System.Drawing.Point(30, 126);
+            this.rDisplay.Name = "rDisplay";
+            this.rDisplay.Size = new System.Drawing.Size(99, 20);
+            this.rDisplay.TabIndex = 1;
+            this.rDisplay.Text = "0";
+            this.rDisplay.TextChanged += new System.EventHandler(this.rDisplay_TextChanged);
+            // 
+            // rLabel
+            // 
+            this.rLabel.AutoSize = true;
+            this.rLabel.Location = new System.Drawing.Point(7, 129);
+            this.rLabel.Name = "rLabel";
+            this.rLabel.Size = new System.Drawing.Size(18, 13);
+            this.rLabel.TabIndex = 2;
+            this.rLabel.Text = "R:";
+            // 
+            // gLabel
+            // 
+            this.gLabel.AutoSize = true;
+            this.gLabel.Location = new System.Drawing.Point(7, 155);
+            this.gLabel.Name = "gLabel";
+            this.gLabel.Size = new System.Drawing.Size(18, 13);
+            this.gLabel.TabIndex = 4;
+            this.gLabel.Text = "G:";
+            // 
+            // gDisplay
+            // 
+            this.gDisplay.Location = new System.Drawing.Point(30, 152);
+            this.gDisplay.Name = "gDisplay";
+            this.gDisplay.Size = new System.Drawing.Size(99, 20);
+            this.gDisplay.TabIndex = 3;
+            this.gDisplay.Text = "0";
+            this.gDisplay.TextChanged += new System.EventHandler(this.gDisplay_TextChanged);
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Location = new System.Drawing.Point(7, 181);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(17, 13);
+            this.bLabel.TabIndex = 6;
+            this.bLabel.Text = "B:";
+            // 
+            // bDisplay
+            // 
+            this.bDisplay.Location = new System.Drawing.Point(30, 178);
+            this.bDisplay.Name = "bDisplay";
+            this.bDisplay.Size = new System.Drawing.Size(99, 20);
+            this.bDisplay.TabIndex = 5;
+            this.bDisplay.Text = "0";
+            this.bDisplay.TextChanged += new System.EventHandler(this.bDisplay_TextChanged);
+            // 
+            // hexLabel
+            // 
+            this.hexLabel.AutoSize = true;
+            this.hexLabel.Location = new System.Drawing.Point(336, 181);
+            this.hexLabel.Name = "hexLabel";
+            this.hexLabel.Size = new System.Drawing.Size(29, 13);
+            this.hexLabel.TabIndex = 8;
+            this.hexLabel.Text = "Hex:";
+            // 
+            // hexDisplay
+            // 
+            this.hexDisplay.Location = new System.Drawing.Point(370, 178);
+            this.hexDisplay.Name = "hexDisplay";
+            this.hexDisplay.Size = new System.Drawing.Size(101, 20);
+            this.hexDisplay.TabIndex = 7;
+            this.hexDisplay.Text = "#";
+            this.hexDisplay.TextChanged += new System.EventHandler(this.hexDisplay_TextChanged);
+            // 
+            // randomColor
+            // 
+            this.randomColor.Location = new System.Drawing.Point(370, 123);
+            this.randomColor.Name = "randomColor";
+            this.randomColor.Size = new System.Drawing.Size(99, 23);
+            this.randomColor.TabIndex = 11;
+            this.randomColor.Text = "Random Color";
+            this.randomColor.UseVisualStyleBackColor = true;
+            this.randomColor.Click += new System.EventHandler(this.randomColor_Click);
+            // 
+            // saveColor
+            // 
+            this.saveColor.BackgroundImage = global::ColorPicker.Properties.Resources.floppy_disk;
+            this.saveColor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.saveColor.Location = new System.Drawing.Point(332, 132);
+            this.saveColor.Name = "saveColor";
+            this.saveColor.Size = new System.Drawing.Size(32, 32);
+            this.saveColor.TabIndex = 13;
+            this.toolTip.SetToolTip(this.saveColor, "Save current color to file");
+            this.saveColor.UseVisualStyleBackColor = true;
+            this.saveColor.Click += new System.EventHandler(this.saveColor_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(15, 35);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(110, 39);
+            this.lblInfo.TabIndex = 14;
+            this.lblInfo.Text = "Press F1 to select \r\nthe color where \r\nyour cursor is pointed.";
+            // 
+            // rSlider
+            // 
+            this.rSlider.AutoSize = false;
+            this.rSlider.Location = new System.Drawing.Point(139, 126);
+            this.rSlider.Maximum = 255;
+            this.rSlider.Name = "rSlider";
+            this.rSlider.Size = new System.Drawing.Size(157, 16);
+            this.rSlider.TabIndex = 15;
+            this.rSlider.TabStop = false;
+            this.rSlider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.rSlider.Scroll += new System.EventHandler(this.rSlider_Scroll);
+            // 
+            // gSlider
+            // 
+            this.gSlider.AutoSize = false;
+            this.gSlider.Location = new System.Drawing.Point(139, 152);
+            this.gSlider.Maximum = 255;
+            this.gSlider.Name = "gSlider";
+            this.gSlider.Size = new System.Drawing.Size(157, 16);
+            this.gSlider.TabIndex = 16;
+            this.gSlider.TabStop = false;
+            this.gSlider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.gSlider.Scroll += new System.EventHandler(this.gSlider_Scroll);
+            // 
+            // bSlider
+            // 
+            this.bSlider.AutoSize = false;
+            this.bSlider.Location = new System.Drawing.Point(139, 178);
+            this.bSlider.Maximum = 255;
+            this.bSlider.Name = "bSlider";
+            this.bSlider.Size = new System.Drawing.Size(157, 16);
+            this.bSlider.TabIndex = 17;
+            this.bSlider.TabStop = false;
+            this.bSlider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.bSlider.Scroll += new System.EventHandler(this.bSlider_Scroll);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 205);
+            this.Controls.Add(this.saveColor);
+            this.Controls.Add(this.randomColor);
+            this.Controls.Add(this.hexLabel);
+            this.Controls.Add(this.hexDisplay);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.bDisplay);
+            this.Controls.Add(this.gLabel);
+            this.Controls.Add(this.gDisplay);
+            this.Controls.Add(this.rLabel);
+            this.Controls.Add(this.rDisplay);
+            this.Controls.Add(this.bSlider);
+            this.Controls.Add(this.gSlider);
+            this.Controls.Add(this.rSlider);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.colorPreview);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Tool";
+            this.colorPreview.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel colorPreview;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.TextBox rDisplay;
+        private System.Windows.Forms.Label rLabel;
+        private System.Windows.Forms.Label gLabel;
+        private System.Windows.Forms.TextBox gDisplay;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.TextBox bDisplay;
+        private System.Windows.Forms.Label hexLabel;
+        private System.Windows.Forms.TextBox hexDisplay;
+        private System.Windows.Forms.Button randomColor;
+        private System.Windows.Forms.Button openColorDialog;
+        private System.Windows.Forms.Button saveColor;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TrackBar rSlider;
+        private System.Windows.Forms.TrackBar gSlider;
+        private System.Windows.Forms.TrackBar bSlider;
+    }
+}
+
---
+++ ColorPicker/MainForm.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ColorPicker
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        private static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        // Windows' hotkey event
+        private static int WM_HOTKEY = 0x312;
+
+        private byte r = 0, g = 0, b = 0;
+        private string hex;
+        private Random randomizer = new Random();
+
+        // Set up the hotkey
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_HOTKEY)
+            {
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+
+                if (key == Keys.F1)
+                {
+                    Point cursor = new Point();
+                    GetCursorPos(ref cursor);
+
+                    r = GetColorAt(cursor).R;
+                    b = GetColorAt(cursor).B;
+                    g = GetColorAt(cursor).G;
+
+                    rDisplay.Text = r.ToString();
+                    gDisplay.Text = g.ToString();
+                    bDisplay.Text = b.ToString();
+
+                    rSlider.Value = r;
+                    gSlider.Value = g;
+                    bSlider.Value = b;
+
+                    convertRGBToHex();
+                }
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            RegisterHotKey(this.Handle, (int)Keys.F1, 0, (uint)Keys.F1);
+
+            generateRandomColor();
+        }
+
+        #region Textboxes
+        private void hexDisplay_TextChanged(object sender, EventArgs e)
+        {
+            hex = hexDisplay.Text;
+
+            //prevents crash on launch from method being called as soon as the random color is created
+            if (hexDisplay.Focused)
+            {
+                convertHexToRGB();
+            }
+        }
+
+        private void rDisplay_TextChanged(object sender, EventArgs e)
+        {
+            byte.TryParse(rDisplay.Text, out r);
+            rSlider.Value = r;
+            convertRGBToHex();
+        }
+
+        private void gDisplay_TextChanged(object sender, EventArgs e)
+        {
+            byte.TryParse(gDisplay.Text, out g);
+            gSlider.Value = g;
+            convertRGBToHex();
+        }
+
+        private void bDisplay_TextChanged(object sender, EventArgs e)
+        {
+            byte.TryParse(bDisplay.Text, out b);
+            bSlider.Value = b;
+            convertRGBToHex();
+        }
+        #endregion
+
+        #region Buttons
+        private void randomColor_Click(object sender, EventArgs e)
+        {
+            generateRandomColor();
+        }
+
+        private void openColorDialog_Click(object sender, EventArgs e)
+        {
+            colorDialog.Color = Color.FromArgb(r, g, b);
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                colorPreview.BackColor = colorDialog.Color;
+
+                r = colorDialog.Color.R;
+                g = colorDialog.Color.G;
+                b = colorDialog.Color.B;
+                rDisplay.Text = colorDialog.Color.R.ToString();
+                gDisplay.Text = colorDialog.Color.G.ToString();
+                bDisplay.Text = colorDialog.Color.B.ToString();
+                rSlider.Value = r;
+                gSlider.Value = g;
+                bSlider.Value = b;
+
+                convertRGBToHex();
+            }
+        }
+
+        private void saveColor_Click(object sender, EventArgs e)
+        {
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Saved Colors.txt";
+
+            if (File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine("R: " + r);
+                    sw.WriteLine("G: " + g);
+                    sw.WriteLine("B: " + b);
+                    sw.WriteLine("Hex: #" + hex);
+                    sw.WriteLine("================");
+                    sw.Close();
+                }
+            }
+            else
+            {
+                var savefile = File.Create(filepath);
+                savefile.Close();
+
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine("R: " + r);
+                    sw.WriteLine("G: " + g);
+                    sw.WriteLine("B: " + b);
+                    sw.WriteLine("Hex: #" + hex);
+                    sw.WriteLine("================");
+                    sw.Close();
+                }
+            }
+        }
+        #endregion
+
+        #region Logic
+        //converts the RGB values to hex, updates the textbox if it's not focused, and displays the color
+        public void convertRGBToHex()
+        {
+            hex = string.Format("{0:X}{1:X}{2:X}", r, g, b);
+
+            if (!hexDisplay.Focused)
+            {
+                hexDisplay.Text = hex;
+            }
+
+            colorPreview.BackColor = Color.FromArgb(r, g, b);
+        }
+
+        //converts hex to RGB, updates the textboxes if they aren't focused, and displays the color
+        public void convertHexToRGB()
+        {
+            try
+            {
+                int num = (int)long.Parse(hex, NumberStyles.HexNumber);
+                r = Convert.ToByte((num & 0xFF0000) >> 16);
+                g = Convert.ToByte((num & 0xFF00) >> 8);
+                b = Convert.ToByte(num & 0xFF);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            if (!rDisplay.Focused || !gDisplay.Focused || !bDisplay.Focused)
+            {
+                rDisplay.Text = r.ToString();
+                gDisplay.Text = g.ToString();
+                bDisplay.Text = b.ToString();
+            }
+
+            colorPreview.BackColor = Color.FromArgb(r, g, b);
+        }
+
+        //generates a random color
+        public void generateRandomColor()
+        {
+            r = Convert.ToByte(randomizer.Next(255));
+            g = Convert.ToByte(randomizer.Next(255));
+            b = Convert.ToByte(randomizer.Next(255));
+
+            rDisplay.Text = r.ToString();
+            gDisplay.Text = g.ToString();
+            bDisplay.Text = b.ToString();
+
+            rSlider.Value = r;
+            gSlider.Value = g;
+            bSlider.Value = b;
+
+            convertRGBToHex();
+        }
+
+        public Color GetColorAt(Point location)
+        {
+            Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+
+            //take an image of the screen and wherever the cursor is, save the color
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            //return the color of whatever pixel the cursor was at (the bitmap is 1x1 so 0,0)
+            return screenPixel.GetPixel(0, 0);
+        }
+        #endregion
+
+        #region Value Sliders
+        private void rSlider_Scroll(object sender, EventArgs e)
+        {
+            rDisplay.Text = rSlider.Value.ToString();
+        }
+
+        private void gSlider_Scroll(object sender, EventArgs e)
+        {
+            gDisplay.Text = gSlider.Value.ToString();
+        }
+
+        private void bSlider_Scroll(object sender, EventArgs e)
+        {
+            bDisplay.Text = bSlider.Value.ToString();
+        }
+        #endregion
+
+        private void MouseMoveTimer_Tick(object sender, EventArgs e)
+        {
+            Point cursor = new Point();
+            GetCursorPos(ref cursor);
+
+            var c = GetColorAt(cursor);
+            this.BackColor = c;
+        }
+    }
+}
